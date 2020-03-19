d11cd601383e5a896dd12ade716e2795940fd910
parmcoder <possawat2017@hotmail.com>
2020-03-20 06:02:32 +0700

    more functions will be finished later, some basic functions are made.

--- PMA-backend/psql_functions.py
+++ PMA-backend/psql_functions.py
@@ -1,5 +1,7 @@
 import psycopg2
 import numpy as np
+import time
+import datetime
 """
 @Parm db_executer - execute on db4parm
 Instruction: db_executer(query,mode,rows)
@@ -152,3 +154,70 @@ def delete_receipt_prescription_table(rids):
         r2_query = "DELETE FROM prescription WHERE rid = "+str(rid)
         db_executer(r2_query, 3, )
         db_executer(r1_query, 3, )
+
+"""
+@Parm expire_compare_bool - compare time if time_first is before time_another
+Instruction: expire_compare_bool(time_first, time_another)
+*RETURN* -> Boolean
+"""
+def expire_compare_bool(t1, t2):
+    t1s = t1.split("-")
+    t2s = t2.split("-")
+    if int(t1s[0]) > int(t2s[0]):
+        return True
+    if int(t1s[1]) > int(t2s[1]):
+        return True
+    if int(t1s[2]) >= int(t2s[2]):
+        return True
+    return False
+
+"""
+@Parm nearest_expire_date - search through the medicine table and show the list of expiring stocks
+Instruction: nearest_expire_date(next_days)
+*RETURN* -> list of [Boolean, tuple] where Boolean stands for expired status
+
+This is advance function, but it is not hard to implement
+"""
+def nearest_expire_date(days=7):
+    named_tuple = time.localtime()  # get struct_time
+    date = datetime.datetime(named_tuple[0], named_tuple[1], named_tuple[2])
+    exp_check = date + datetime.timedelta(days)
+    time_string = exp_check.strftime('%Y-%m-%d')
+    present_string = date.strftime('%Y-%m-%d')
+    expire_query = "SELECT * FROM medicine where exp_date < \'"+time_string+"\' order by exp_date desc"
+    resultset = db_executer(expire_query, 1, )
+
+    expires = []
+    for i in resultset:
+        if expire_compare_bool(present_string, str(i[1])):
+            expires.append([True,i])
+        else:
+            expires.append([False,i])
+    # for i in expires:
+    #     print(i)
+    return expires
+
+"""
+@Parm predict_restock - search through the receipt table and prescription 
+then show the list of stocks that needed to be restock according to the trend.
+
+About the product popularity trend, we find the top 10 most purchased drug on the last 3 month and let them be popular drugs.
+About the sales trend, we plot the graph by using total earning in each month to predict how much do we need to pre-purchase the drugs.
+
+With 2 data, we assume that the more we earn in the previous month, the more we will purchase the drugs.
+Also, the basic formula is as follow :
+    amount_to_buy  = base_stock + amount_sold + amount_sold/(2.5*ranking)                 if on the top 3
+                   = base_stock + int((amount_sold)*trend_slope)                          if on the top 10, but not top 3
+                   = base_stock - amount_left                                             otherwise           
+trend_slope is predicted using linear regression on the trend with straight line.
+base_stock is around 30, depends on the shop size.
+
+Instruction: predict_restock(year,month,base_stock) where year and month comes from the date we want to restock.
+The lastest timestamp is 2020-12-30. Beyond that won't show more graph, so assume the date you were at the moment.
+*RETURN* -> list of [integer, tuple] where integer stands for amount of drugs needed to be bought and list of pivots for the graph (date, earning)
+
+This is advance function calculate the shortage of stocks and maximize profit
+"""
+def predict_restock():
+
+    return 1
